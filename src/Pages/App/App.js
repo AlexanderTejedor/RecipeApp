e564b0d9db8d98baaf472541786ca98437db6c22
Alexander Tejedor <atromero.98@gmail.com>
2025-06-05 11:23:21 -0500

    feat: configure Nativewind with Babel, Tailwind CSS, CSS, and Metro setups

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "../../../global.css";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Recetas por Categoria</Text>
+      <Text className="text-3xl text-white">Recetas por Categoria</Text>
+      <View>
+        <Text className="text-red-500">Vamos Funciona</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
